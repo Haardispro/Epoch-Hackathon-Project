686e5b4ef64cdfe2e40d9186e5211090be8e2375
Haardispro <haardmajmudar2827@gmail.com>
2023-01-01 07:47:39 +0530

    added more more stuff

--- audiomixer.py
+++ audiomixer.py
@@ -12,7 +12,7 @@ w = Tk()
 
 w.title("Audio Mixer")
 w.config(bg="#282828")
-
+w.resizable(height=False, width=False)
 def open_html():
     os.system('brave-browser index.html')
 
@@ -71,6 +71,9 @@ def overlay_button():
     play_overlayed_song.grid(row=4, column=1, padx=10, pady=10)
     os.system("sudo rm temp/*")
 
+def open_link():
+    os.system("python audiomixerlink.py")
+
 """
     path = "temp/"
     for file_name in os.listdir(path):
@@ -86,7 +89,7 @@ fonts = ("Cascadia Code", "18")
 title_label = Label(w, text="Audio Mixer", font=fonts, fg="white", bg="#282828")
 title_label.grid(row=0, column=1, padx=10, pady=10)
 
-background_music_label = Label(w, text="Enter Background Music \nLink(Youtube only): ", font=fonts, fg="white", bg="#282828")
+background_music_label = Label(w, text="Enter Background Music \nname(Youtube): ", font=fonts, fg="white", bg="#282828")
 background_music_label.grid(row=1, column=0, padx=10, pady=10)
 
 background_music_entry = Entry(w, font=fonts, width=25)
@@ -96,7 +99,7 @@ background_music_entry.grid(row=1, column=1, padx=10, pady=10)
 background_music_download = Button(w, text="Download Background track", font=fonts, command=download_background_music)
 background_music_download.grid(row=1, column=2, padx=10, pady=10)
 
-vocals_label = Label(w, text="Enter Vocals \nLink(Youtube only): ", font=fonts, fg="white", bg="#282828")
+vocals_label = Label(w, text="Enter Vocals \nname(Youtube): ", font=fonts, fg="white", bg="#282828")
 vocals_label.grid(row=2, column=0, padx=10, pady=10)
 
 vocals_entry = Entry(w, font=fonts, width=25)
@@ -110,6 +113,7 @@ vocals_download.grid(row=2, column=2, padx=10, pady=10)
 overlay_process_button = Button(w, text="Start", font=fonts, command=overlay_button)
 overlay_process_button.grid(row=3, column=1, padx=10, pady=10)
 
-
+link_button = Button(text="Link Feature", font=fonts, command=open_link)
+link_button.grid(row=4, column=1, padx=10, pady=10)
 
 w.mainloop()
